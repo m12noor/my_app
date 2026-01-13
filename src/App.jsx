9b1b3a7964bd2bc0638a20293b8d55270f2fb565
m12noor <mahnoorriaz540@gmail.com>
2026-01-13 15:49:51 +0500

    fixed syntax error in App.jxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react'
-=======
-import React from 'react'
->>>>>>> 5f95a22d7716f211fe09664ee51718e88a19fceb
 import Header from './Header.jsx'
 import Banner from './Banner.jsx'
 import Services from './Services.jsx'
@@ -10,7 +6,6 @@ import ProjectTable from './ProjectTable.jsx'
 import Contactform from './Contactform.jsx'
 import Footer from './Footer.jsx'
 function App() {
-<<<<<<< HEAD
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userEmail, setUserEmail] = useState('');
 
@@ -56,13 +51,6 @@ function App() {
 
       <Banner />
       <Services isLoggedIn={isLoggedIn} />
-=======
-  return (
-    <div>
-      < Header />
-      <Banner />
-      <Services />
->>>>>>> 5f95a22d7716f211fe09664ee51718e88a19fceb
       <ProjectTable />
       <Contactform />
       <Footer />
